98713fc3908e905b5f57fd90e163b655cc6993c3
Olemis Lang <olemis@simelo.tech>
2019-02-27 14:46:10 -0500

    [ci] refs #1 - Activate Python 3.5.0 on Travis osx builds

--- ci-scripts/install-osx.sh
+++ ci-scripts/install-osx.sh
@@ -2,11 +2,16 @@
 
 set -x
 
-brew update;
+brew update
 
 # Install protobuf
-brew install protobuf protobuf-c;
+brew install protobuf protobuf-c
 
-#Install gimme
-brew install gimme;
+# Install gimme
+brew install gimme
+
+# Install Python 3.5
+brew install pyenv
+brew install pyenv-virtualenv
+pyenv install 3.5.0
 
