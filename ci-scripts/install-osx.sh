d168eb0f8c27d79e2b2e6fcbadc19a30d9ee1eb0
Olemis Lang <olemis@simelo.tech>
2019-04-05 16:53:35 -0400

    [ci] Reinstall openssl before pyenv.

--- ci-scripts/install-osx.sh
+++ ci-scripts/install-osx.sh
@@ -14,9 +14,13 @@ brew install gimme
 if [ -z "$PYTHON_VERSION"] ; then
   export PYTHON_VERSION=3.5.0
 fi
-brew install pyenv
+brew uninstall openssl
+brew install openssl sqlite pyenv
 brew install pyenv-virtualenv
 echo "Installing Python=$PYTHON_VERSION"
 OPENSSL_PATH="$(brew --prefix openssl)"
-LDFLAGS="-L$OPENSSL_PATH/lib" CPPFLAGS="-I$OPENSSL_PATH/include" CFLAGS="-I$OPENSSL_PATH/include" pyenv install $PYTHON_VERSION
+CFLAGS="-I$(brew --prefix openssl)/include -I$(brew --prefix sqlite)/include" \
+CPPFLAGS="-I$(brew --prefix openssl)/include -I$(brew --prefix sqlite)/include" \
+LDFLAGS="-L$(brew --prefix openssl)/lib -L$(brew --prefix sqlite)/lib" \
+pyenv install -v $PYTHON_VERSION
 
