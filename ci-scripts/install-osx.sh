a54547e49d38cd9b2b9a9ff10334099322e17ba2
Olemis Lang <olemis@simelo.tech>
2019-04-07 16:16:10 -0400

    [ci] Do not use pyenv on Travis osx builds since Python=3.7 installed out of the box ...
    ... and using pyenv leads to failures

--- ci-scripts/install-osx.sh
+++ ci-scripts/install-osx.sh
@@ -10,19 +10,3 @@ brew install protobuf protobuf-c
 # Install gimme
 brew install gimme
 
-# Install Python
-# if [ -z "$PYTHON_VERSION"] ; then
-#   export PYTHON_VERSION=3.5.0
-# fi
-# brew install openssl sqlite pyenv
-# brew upgrade openssl sqlite pyenv
-# brew link --force openssl
-# brew link --force sqlite
-# brew install pyenv-virtualenv
-# echo "Installing Python=$PYTHON_VERSION"
-# OPENSSL_PATH="$(brew --prefix openssl)"
-# CFLAGS="-I$(brew --prefix openssl)/include -I$(brew --prefix sqlite)/include" \
-# CPPFLAGS="-I$(brew --prefix openssl)/include -I$(brew --prefix sqlite)/include" \
-# LDFLAGS="-L$(brew --prefix openssl)/lib -L$(brew --prefix sqlite)/lib" \
-# pyenv install -v $PYTHON_VERSION
-
